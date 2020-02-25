3d9a8b5d346128afe356718679e4e2381782a42a
omar <omar160199@gmail.com>
2020-02-26 01:01:05 +0200

    add child

--- app/src/main/java/com/appz/qrcode/client_tasks/profileTaps/addActivity.java
+++ app/src/main/java/com/appz/qrcode/client_tasks/profileTaps/addActivity.java
@@ -35,9 +35,17 @@ public class addActivity extends AppCompatActivity {
     FirebaseDatabase database ;
     DatabaseReference ref ;
     FirebaseAuth auth;
+    private final DatabaseReference rationTable = FirebaseDatabase.getInstance().getReference().child(AllFinal.Ration_Data);
+    private final DatabaseReference fakeTable = FirebaseDatabase.getInstance().getReference().child(AllFinal.FAKE_DATA);
+
 
     //vars
     int points ;
+    SharedPreferences sharedPreferences ;
+    String parent ;
+    List ids = new ArrayList();
+    List correctIds = new ArrayList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +55,21 @@ public class addActivity extends AppCompatActivity {
         init();
         findByID();
         addChild();
+
     }
+
+
 
     private void init()
     {
         database = FirebaseDatabase.getInstance();
         ref = database.getReference().child(AllFinal.Ration_Data);
         auth = FirebaseAuth.getInstance();
-
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        parent = sharedPreferences.getString("parent1","");
+        getdata(ref.child(parent).child("points"));
+        getIDs(rationTable.child(parent));
+        getCorrectIDs(fakeTable.child(parent).child(AllFinal.CHILDS));
     }
 
     private void findByID()
@@ -65,7 +80,7 @@ public class addActivity extends AppCompatActivity {
     }
 
 
-    private int getdata(DatabaseReference reff)
+    private void getdata(DatabaseReference reff)
     {
         reff.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -79,7 +94,6 @@ public class addActivity extends AppCompatActivity {
             }
         });
 
-        return points;
     }
 
     private void addChild()
@@ -90,22 +104,117 @@ public class addActivity extends AppCompatActivity {
                 String ID = id.getText().toString().trim();
                 String Name = name.getText().toString().trim();
 
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                String parent = sharedPreferences.getString("parent1","");
+                if (isCorrect(ID))
+                {
 
 
-                ref.child(parent).child(ID);
-                ref.child(parent).child(ID).child("Name").setValue(Name);
+                    if(isAlreadyExist(ID))
+                    {
+                        Toast.makeText(getBaseContext(),"This ID is Already Exist",Toast.LENGTH_LONG).show();
+                    }else
+                    {
+                        points+=50;
+                        ref.child(parent).child(ID);
+                        ref.child(parent).child(ID).child("Name").setValue(Name);
+                        ref.child(parent).child("points").setValue(points);
+                        Toast.makeText(getBaseContext(),"Success",Toast.LENGTH_LONG).show();
+                    }
 
-                points =  getdata(ref.child(parent).child("points"));
 
-                points+=50;
+                }else
+                {
+                    Toast.makeText(getBaseContext(),"Enter Correct ID",Toast.LENGTH_LONG).show();
+                }
 
-                ref.child(parent).child("points").setValue(points);
+
 
             }
         });
 
 
+    }
+
+    private void getIDs (DatabaseReference ref)
+    {
+
+        ids.clear();
+        ref.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    ids.add(data.getKey());
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getBaseContext(),databaseError.getMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+    }
+
+    private boolean isAlreadyExist(String id)
+    {
+
+
+        for ( int i = 0 ; i <ids.size();++i)
+        {
+            if(ids.get(i).equals(id))
+                return true;
+        }
+        return false ;
+    }
+
+    private void getCorrectIDs (DatabaseReference ref)
+    {
+
+        correctIds.clear();
+        ref.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    correctIds.add(data.getKey());
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getBaseContext(),databaseError.getMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+    }
+
+    private boolean isCorrect(String id)
+    {
+        for ( int i = 0 ; i <correctIds.size();++i)
+        {
+            if(correctIds.get(i).equals(id))
+                return true;
+        }
+        return false ;
     }
 }
