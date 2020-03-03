eeaf12cabb127a06e9d1a890628ab6cb601793ae
omar <omar160199@gmail.com>
2020-03-03 08:26:33 +0200

    ahmed hassan

--- app/src/main/java/com/appz/qrcode/seller_tasks/ConfirmActivity.java
+++ app/src/main/java/com/appz/qrcode/seller_tasks/ConfirmActivity.java
@@ -3,14 +3,9 @@ package com.appz.qrcode.seller_tasks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ActionMode;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.appz.qrcode.R;
 import com.appz.qrcode.helperUi.AllFinal;
@@ -23,6 +18,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ConfirmActivity extends AppCompatActivity {
 
